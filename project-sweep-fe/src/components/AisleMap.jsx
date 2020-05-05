fcbd02562f494d169762457dbbede61961c81e81
higashiM <eastwg@gmail.com>
2020-05-05 10:57:42 +0100

    coneect aisle map

--- project-sweep-fe/src/components/AisleMap.jsx
+++ project-sweep-fe/src/components/AisleMap.jsx
@@ -2,12 +2,18 @@ import React, { Component } from 'react'
 import { Link } from '@reach/router'
 
 class AisleMap extends Component {
-    state = { currentAisle: 1 }
     render() {
-        const map = this.props.pathMaps.pathMaps[this.state.currentAisle]
+        const { aisleCount, aisleOrder, pathMaps } = this.props
+        const thisAisle = aisleOrder[aisleCount - 1]
+        const nextAisle = aisleOrder[aisleCount]
+
+        const map = pathMaps[thisAisle]
 
         return (
             <div className="aisleMap">
+                <h2>
+                    Aisle {thisAisle} going to {nextAisle}
+                </h2>
                 <>{map}</>
                 <Link to="/aisleList" className="shoppingListCompleteButton">
                     Next list...
