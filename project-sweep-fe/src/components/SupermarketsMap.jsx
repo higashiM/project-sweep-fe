852477d91ef0b59f65fbe13c37399ea6881c2c96
AVSheard <a_sheard@yahoo.co.uk>
2020-05-06 08:39:29 +0100

    added SupermarketList functionality to supermarket map page

--- project-sweep-fe/src/components/SupermarketsMap.jsx
+++ project-sweep-fe/src/components/SupermarketsMap.jsx
@@ -1,8 +1,41 @@
 import React, { Component } from 'react'
+import { Link } from '@reach/router'
+import Loader from './Loader'
+import * as api from '../utils/api'
 
 class SupermarketsMap extends Component {
+    state = { isLoading: true, supermarkets: [] }
+
+    componentDidMount() {
+        api.getSupermarkets().then(({ supermarkets }) => {
+            this.setState({ supermarkets, isLoading: false })
+        })
+    }
+
+    handleClick = (supermarket) => {
+        this.props.setSupermarket(supermarket)
+    }
+
     render() {
-        return <div></div>
+        const { supermarkets, isLoading } = this.state
+        if (isLoading) return <Loader />
+        return (
+            <div className="notepad">
+                <h2>Supermarket Selector</h2>
+                {supermarkets.map((supermarket) => {
+                    return (
+                        <Link
+                            onClick={() => this.handleClick(supermarket)}
+                            key={supermarket._id}
+                            to="/shopmap"
+                            className="shoppingListCompleteButton"
+                        >
+                            {supermarket.name}
+                        </Link>
+                    )
+                })}
+            </div>
+        )
     }
 }
 
